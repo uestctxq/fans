baa88a4567fb15c77f6e18a29aab54d20653d8fd
xiaoqingtang214445 <txqyou@gmail.com>
2018-09-18 09:46:34 +0800

    bugfix

--- project/get_data/fensicount.py
+++ project/get_data/fensicount.py
@@ -10,9 +10,17 @@ def get_fen_count_by_brand(**params):
     today = datetime.datetime.strptime(now, '%Y-%m-%d %H:%M:%S')
     r = requests.get(params['url'])
     soup = BeautifulSoup(r.text, "lxml")
+    times = 0
     while soup.find(id="followCount") is None:
         r = requests.get(params['url'])
         soup = BeautifulSoup(r.text, "lxml")
-    count = soup.find(id="followCount").attrs['value']
-    db.session.add(FenSi(company= params['company'], count=count, updated=today, html=r.text))
+        if times == 10 :
+            break
+        times = times + 1
+    if times < 10 :
+        count = soup.find(id="followCount").attrs['value']
+    else :
+        count = soup.find(class_="shop_profile_extra").text.split("\n")[2].split(u"万")[0]
+
+    db.session.add(FenSi(company= params['company'], count=count, updated=today, html=soup.find(class_="shop_profile_extra").text.split("\n")[2] or r.text))
     db.session.commit()
